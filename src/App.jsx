dc8ae24d69ce67f5f5628da91dd5ae3e5f4e1413
UliaKarpova <chelovek-e-95@mail.ru>
2023-11-06 20:29:12 +0300

    create Auth component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import Button from "./components/ui/Button/Button.jsx";
-import Input from "./components/ui/Input/Input.jsx";
+import Auth from './components/Auth/Auth'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return (
     <main className='app'>
-      <h1>Регистрация</h1>
-      <Button>Войти</Button>
-        <Input placeholder='Логин'/>
-        <Input placeholder='Пароль' forPassword/>
+      {!isLoggedIn && <Auth />}
     </main>
   )
 }

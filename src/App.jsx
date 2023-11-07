d91eeac571b03242dc56a71b93773ce22e0fd149
UliaKarpova <chelovek-e-95@mail.ru>
2023-11-07 20:30:26 +0300

    create managerHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Auth from './components/Auth/Auth'
+import WorkersList from './components/WorkersList/WorkersList'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return (
     <main className='app'>
-      {!isLoggedIn && <Auth />}
+      {/* {!isLoggedIn && <Auth />} */}
+      <WorkersList />
     </main>
   )
 }
---
+++ src/components/WorkersList/WorkersList.jsx
@@ -0,0 +1,35 @@
+import logo from '../../images/logo.png'
+import allert from '../../images/allert.svg'
+import Button from "../ui/Button/Button.jsx";
+import Input from "../ui/Input/Input.jsx";
+import ManagerHeader from '../ManagerHeader/ManagerHeader';
+import './WorkersList.css'
+
+function WorkersList() {
+  return (
+    <>
+      <ManagerHeader />
+      {/* <img class='logo'
+        src={logo}
+        alt='Логотип Совкомбанка' />
+      <h1 class='title'>Авторизация</h1>
+      <form class='form'>
+        <Input style={{ with: '100%' }} placeholder='Логин' />
+        <Input placeholder='Пароль' forPassword />
+        <div class='warning'>
+          <img class='warning_img'
+            src={allert}
+            alt='Восклицательный знак' />
+          <span class='warning_text'>
+            Для регистрации в приложении<br />обратитесь к менеджеру
+          </span>
+        </div>
+        <Button style={{marginTop: 'auto'}}>Войти</Button>
+
+      </form> */}
+
+    </>
+  )
+}
+
+export default WorkersList
